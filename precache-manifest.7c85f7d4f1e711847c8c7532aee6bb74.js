bbcf694bb3889a0b45514da423dd99df9940ff4a
RamlanRamdani27 <63625941+RamlanRamdani27@users.noreply.github.com>
2022-02-20 17:42:58 +0700

    Add files via upload

---
+++ precache-manifest.7c85f7d4f1e711847c8c7532aee6bb74.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc1ed99e9cdb8e3480b",
+    "url": "/css/about.560be4cd.css"
+  },
+  {
+    "revision": "e63dc5c469e9005a1343",
+    "url": "/css/chunk-vendors.dbae5ec2.css"
+  },
+  {
+    "revision": "ab192990332a175f1cfc",
+    "url": "/css/index.dc53eaa0.css"
+  },
+  {
+    "revision": "0fbfc862056bad982c93ba51460e31d9",
+    "url": "/fonts/fa-brands-400.0fbfc862.ttf"
+  },
+  {
+    "revision": "a9afdb72826cde196ddf29eb8f9d0f8f",
+    "url": "/fonts/fa-brands-400.a9afdb72.woff2"
+  },
+  {
+    "revision": "35307505c129ab1f9173f7917b50756f",
+    "url": "/fonts/fa-regular-400.35307505.ttf"
+  },
+  {
+    "revision": "f817938f131b0cabee81e59a96f9c2a6",
+    "url": "/fonts/fa-regular-400.f817938f.woff2"
+  },
+  {
+    "revision": "297973a488f688271dd223d542ba2697",
+    "url": "/fonts/fa-solid-900.297973a4.woff2"
+  },
+  {
+    "revision": "ffc89d3ea2e2c3ba295689305f91ca78",
+    "url": "/fonts/fa-solid-900.ffc89d3e.ttf"
+  },
+  {
+    "revision": "4bde6a62454b669bb0ce0448491a71d7",
+    "url": "/fonts/fa-v4compatibility.4bde6a62.ttf"
+  },
+  {
+    "revision": "72d85419b5a3c5867b1903ef565f5a8d",
+    "url": "/fonts/fa-v4compatibility.72d85419.woff2"
+  },
+  {
+    "revision": "b53179b22df3e3c44875fb6ba317e2a7",
+    "url": "/img/PP.jpg"
+  },
+  {
+    "revision": "e02c09a365cad0e6fb91f986becf97fa",
+    "url": "/img/svg/book-svgrepo-com.svg"
+  },
+  {
+    "revision": "75822588e1551bfff6768ec70b56f5f9",
+    "url": "/img/svg/campus-svgrepo-com.svg"
+  },
+  {
+    "revision": "335b4f527c1fa36e1db1e94a95c22803",
+    "url": "/img/svg/collage-svgrepo-com.svg"
+  },
+  {
+    "revision": "dadd19b881aac4502cc897bede4bae9c",
+    "url": "/img/svg/lumen-svgrepo-com.svg"
+  },
+  {
+    "revision": "aebb497c6f0d3da9ec2c80e505924e5f",
+    "url": "/img/svg/music-svgrepo-com.svg"
+  },
+  {
+    "revision": "fccafb1b5b1c711f9653200e1b50f82a",
+    "url": "/img/svg/nodejs-icon-logo-svgrepo-com.svg"
+  },
+  {
+    "revision": "aeec9b39a0bfb88d077f22b1e9f29509",
+    "url": "/img/svg/reactjs-svgrepo-com.svg"
+  },
+  {
+    "revision": "2e03b699f9221d40dcce5bb1c8149f98",
+    "url": "/img/svg/school-svgrepo-com.svg"
+  },
+  {
+    "revision": "82b894e70a2a88754fd6a5b7fa753c11",
+    "url": "/img/svg/stock-svgrepo-com.svg"
+  },
+  {
+    "revision": "488c73fb270413b85ec423f28c26d2cb",
+    "url": "/img/svg/store-svgrepo-com.svg"
+  },
+  {
+    "revision": "85adc4eef28ea32d2e204e4936e90578",
+    "url": "/img/svg/store.svg"
+  },
+  {
+    "revision": "8094a03ab736aa701d5eaec5ff21ced5",
+    "url": "/img/svg/university-svgrepo-com.svg"
+  },
+  {
+    "revision": "5021147137c9f915a4559a1605321e4e",
+    "url": "/img/svg/vue-svgrepo-com.svg"
+  },
+  {
+    "revision": "8fa7f45cf06769780008442e0ab72714",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fc1ed99e9cdb8e3480b",
+    "url": "/js/about.8f9f764e.js"
+  },
+  {
+    "revision": "e63dc5c469e9005a1343",
+    "url": "/js/chunk-vendors.7ac4cdc0.js"
+  },
+  {
+    "revision": "ab192990332a175f1cfc",
+    "url": "/js/index.a20bfe8e.js"
+  },
+  {
+    "revision": "1770c2524b653c4741436956a1f7f256",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
